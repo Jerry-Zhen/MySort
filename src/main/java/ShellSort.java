ee94cd40a1024ea9a64dd9996afe8ae70cfbe752
Jerry Chen <chenzhen@vcredit.com>
2018-03-05 14:12:07 +0800

    first comments

--- src/main/java/ShellSort.java
+++ src/main/java/ShellSort.java
@@ -7,26 +7,29 @@ import java.util.Arrays;
 public class ShellSort {
 
     public static void main(String[] args){
-        int[] data = new int[] { 26, 53, 67, 48, 57, 13, 48, 32, 60, 50 };
+        int[] data = new int[] { 26, 53, 67, 48, 57, 13, 48, 32, 60};
         shellSortSmallToBig(data);
         System.out.println(Arrays.toString(data));
     }
 
     public static void shellSortSmallToBig(int[] data){
-        int j = 0;
-        int temp = 0;
-        for(int increment = data.length/2; increment > 0; increment /= 2 ){
-            System.out.println("increment is: "+increment);
-            for(int i = increment; i<data.length;i++){
-                temp = data[i];
-                for(j=i-increment; j>=0; j--){
-                    if(temp < data[j]){
-                        data[j+1] = data[j];
-                    }else{
-                        break;
+        //获取数组的长度
+        int length = data.length;
+        //采用step/2，（step/2）/2。。。的希尔序列步长进行数组的分组
+        for(int step = length/2; step>0; step /=2){
+            System.out.println("step is ..." + step);
+            //循环每一个分组的数组
+            for(int i=0; i<step; i++){
+                //对每一个分组的子数组进行插入排序
+                for(int j = i+step; j<length; j+=step){
+                    int temp = data[j];
+                    int k = j-step;
+                    while(k>=0 && data[k]>temp){
+                        data[k+step] = data[k];
+                        k-=step;
                     }
+                    data[k+step]=temp;
                 }
-                data[j+increment]=temp;
             }
         }
         for(int i=0; i< data.length; i++){
